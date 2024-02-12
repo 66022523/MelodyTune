a2e75c793c935f7cb27cb59decd26e827bde65ba
ice6504 <aphinan6504@gmail.com>
2024-02-12 11:32:49 +0700

    Fix UX signin&signup

--- pages/signin.vue
+++ pages/signin.vue
@@ -5,27 +5,42 @@
       <form class="flex flex-col w-fit add-from">
         <div>
           <h1 class="text-xl font-bold mt-3 text-primary">E-mail</h1>
+          <div class="w-96 bg-white rounded-lg pl-5">
           <input
-            class="bg-white justify-center flex h-12 rounded-lg outline-none placeholder-primary font-medium text-primary w-96"
+            class="bg-white justify-center flex h-12 rounded-lg outline-none placeholder-primary font-medium text-primary w-full"
             type="email"
             required
             v-model="email"
           />
-        </div>
+        </div></div>
         <h1
           class="text-md text-red-600 font-medium mt-1"
           v-if="email.length !== 0"
         >
           {{ errorMessageEmail }}
         </h1>
-        <div class="mb-3">
+        <div class="mb-2">
           <h1 class="text-lg font-bold mt-3 text-primary">Password</h1>
+          <div class="w-96 bg-white rounded-lg pl-5">
           <input
-            class="bg-white h-12 w-96 rounded-lg outline-none text-primary"
+            class="bg-white h-12 w-full rounded-lg outline-none text-primary"
             type="password"
-          />
+            v-model="password"
+          /></div>
         </div>
-        <div class="flex justify-end text-md font-bold text-primary">
+        <p
+          class="text-md text-red-600 font-medium mt-1"
+          v-if="password.length !== 0"
+        >
+          {{ errorMessagePasswordEnglish }}
+        </p>
+        <p
+          class="text-md text-red-600 font-medium mt-1"
+          v-if="password.length !== 0"
+        >
+          {{ errorMessagePasswordLength }}
+        </p>
+        <div class="flex justify-end text-md font-bold text-primary mr-4">
           <NuxtLink to="/signup">Sign Up</NuxtLink>
         </div>
         <div class="flex justify-center">
@@ -36,7 +51,7 @@
             Sign In
           </button>
         </div>
-        <img class="mx-auto mt-3 w-44" src="/img/or.png" />
+        <img class="mx-auto my-3 w-44" src="/img/or.png" />
         <div class="justify-center flex">
           <NuxtLink to="#">
             <img class="w-12 mr-2 rounded-full" src="/img/google.png"
@@ -56,6 +71,9 @@ export default {
     return {
       email: "",
       errorMessageEmail: "",
+      password: "",
+      errorMessagePasswordLength: "",
+      errorMessagePasswordEnglish: "",
     };
   },
   methods: {
@@ -70,11 +88,30 @@ export default {
         this.errorMessageEmail = "อีเมลไม่ถูกต้อง";
       }
     },
+    validatePassword(password) {
+      const isEnglish = /^[A-Za-z0-9#?!@$%^&*-]+$/.test(password);
+      if (!isEnglish) {
+        this.errorMessagePasswordEnglish = "กรุณาเปลี่ยนภาษาเป็น Eng";
+        this.errorMessagePasswordLength = "";
+      } else {
+        this.errorMessagePasswordEnglish = "";
+        if (password.length < 8) {
+          let least = 8 - password.length;
+          this.errorMessagePasswordLength = "ขาดอีก " + least + " ตัวอักษร";
+        } else {
+          this.errorMessagePasswordLength = "";
+        }
+      }
+    },
   },
   watch: {
     email(value) {
       this.email = value;
       this.validateEmail(value);
+    },
+    password(value) {
+      this.password = value;
+      this.validatePassword(value);
     },
   },
 };
--- pages/signup.vue
+++ pages/signup.vue
@@ -56,6 +56,12 @@
           class="text-md text-red-600 font-medium mt-1"
           v-if="password.length !== 0"
         >
+          {{ errorMessagePasswordEnglish }}
+        </p>
+        <p
+          class="text-md text-red-600 font-medium mt-1"
+          v-if="password.length !== 0"
+        >
           {{ errorMessagePasswordLength }}
         </p>
         <p
@@ -104,98 +110,6 @@
           {{ errorMessageConfirm }}
         </span>
 
-        <div>
-          <select
-            class="bg-white h-12 rounded-lg mt-5 w-32 font-medium"
-            name="day"
-            id="day"
-            placeholder="day"
-          >
-            <option value="day">DAY</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-            <option value="19">19</option>
-            <option value="20">20</option>
-            <option value="21">21</option>
-            <option value="22">22</option>
-            <option value="23">23</option>
-            <option value="24">24</option>
-            <option value="25">25</option>
-            <option value="26">26</option>
-            <option value="27">27</option>
-            <option value="28">28</option>
-            <option value="29">29</option>
-            <option value="30">30</option>
-            <option value="31">31</option>
-          </select>
-          <select
-            class="bg-white h-12 rounded-lg mt-5 ml-3 w-32 font-medium"
-            name="month"
-          >
-            <option>MONTH</option>
-            <option value="january">JANUARY</option>
-            <option value="february">FEBRUARY</option>
-            <option value="march">MARCH</option>
-            <option value="april">APRIL</option>
-            <option value="may">MAY</option>
-            <option value="june">JUNE</option>
-            <option value="july">JULY</option>
-            <option value="august">AUGUST</option>
-            <option value="september">SEPTEMBER</option>
-            <option value="october">OCTOBER</option>
-            <option value="november">NOVEMBER</option>
-            <option value="december">DECEMBER</option>
-          </select>
-          <select
-            class="bg-white h-12 rounded-lg mt-5 ml-3 w-32 font-medium"
-            name="year"
-          >
-            <option>YEAR</option>
-            <option value="2000">2000</option>
-            <option value="2001">2001</option>
-            <option value="2002">2002</option>
-            <option value="2003">2003</option>
-            <option value="2004">2004</option>
-            <option value="2005">2005</option>
-            <option value="2006">2006</option>
-            <option value="2007">2007</option>
-            <option value="2008">2008</option>
-            <option value="2009">2009</option>
-            <option value="2010">2010</option>
-            <option value="2011">2011</option>
-            <option value="2012">2012</option>
-            <option value="2013">2013</option>
-            <option value="2014">2014</option>
-            <option value="2015">2015</option>
-            <option value="2016">2016</option>
-            <option value="2017">2017</option>
-            <option value="2018">2018</option>
-            <option value="2019">2019</option>
-            <option value="2020">2020</option>
-            <option value="2021">2021</option>
-            <option value="2022">2022</option>
-            <option value="2023">2023</option>
-            <option value="2024">2024</option>
-            <option value="2025">2025</option>
-          </select>
-        </div>
-
         <div class="justify-center flex">
           <button
             type="submit"
@@ -221,6 +135,7 @@ export default {
       errorMessagePasswordLower: "",
       errorMessagePasswordNumber: "",
       errorMessagePasswordSpecial: "",
+      errorMessagePasswordEnglish: "",
       confirmPassword: "",
       errorMessageConfirm: "",
     };
@@ -242,31 +157,38 @@ export default {
       const containsLowercase = /[a-z]/.test(password);
       const containsNumber = /[0-9]/.test(password);
       const containsSpecial = /[#?!@$%^&*-]/.test(password);
+      const isEnglish = /^[A-Za-z0-9#?!@$%^&*-]+$/.test(password);
 
       this.errorMessagePasswordUpper = "";
       this.errorMessagePasswordLower = "";
       this.errorMessagePasswordNumber = "";
       this.errorMessagePasswordSpecial = "";
 
-      if (password.length < 8) {
-        this.errorMessagePasswordLength =
-          "รหัสผ่านต้องมีความยาวอย่างน้อย 8 ตัวอักษร";
-      }
-      if (!containsUppercase) {
-        this.errorMessagePasswordUpper =
-          "รหัสผ่านต้องมีอักษรตัวใหญ่อย่างน้อยหนึ่งตัว";
-      }
-      if (!containsLowercase) {
-        this.errorMessagePasswordLower =
-          "รหัสผ่านต้องมีอักษรตัวเล็กอย่างน้อยหนึ่งตัว";
-      }
-      if (!containsNumber) {
-        this.errorMessagePasswordNumber =
-          "รหัสผ่านต้องมีตัวเลขอย่างน้อยหนึ่งตัว";
-      }
-      if (!containsSpecial) {
-        this.errorMessagePasswordSpecial =
-          "รหัสผ่านต้องมีอักษรพิเศษอย่างน้อยหนึ่งตัว";
+      if (!isEnglish) {
+        this.errorMessagePasswordEnglish = "กรุณาเปลี่ยนภาษาเป็น Eng";
+        this.errorMessagePasswordLength = "";
+      } else {
+        this.errorMessagePasswordEnglish = "";
+        if (password.length < 8) {
+          let least = 8 - password.length;
+          this.errorMessagePasswordLength = "ขาดอีก " + least + " ตัวอักษร";
+        } else {
+          this.errorMessagePasswordLength = "";
+        }
+        if (!containsUppercase) {
+          this.errorMessagePasswordUpper = "อักษรตัวใหญ่อย่างน้อย 1 ตัว [A-Z]";
+        }
+        if (!containsLowercase) {
+          this.errorMessagePasswordLower =
+            "อักษรตัวเล็กอย่างน้อยหนึ่งตัว [a-z]";
+        }
+        if (!containsNumber) {
+          this.errorMessagePasswordNumber = "ตัวเลขอย่างน้อย 1 ตัว [0-9]";
+        }
+        if (!containsSpecial) {
+          this.errorMessagePasswordSpecial =
+            "อักษรพิเศษอย่างน้อย 1 ตัว [#?!@$%^&*-]";
+        }
       }
     },
     validateConfirmPassword(confirmPassword, password) {
